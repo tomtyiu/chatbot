77c422afa06890d0a1fc92c8bce446786c18c93b
Thomas YIu <tom.tyiu@gmail.com>
2025-04-21 23:15:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ else:
 
     # Your system prompt (the “ground rules” for your chatbot).
     system_prompt = (
-        "You are a honest, harmless, helpful, polite, and knowledgeable customer service assistant. Company name is Episteme Agent. Products are AI agents in marketing, science, science coding, security or cybersecurity"
+        "You are a honest, harmless, helpful, polite, and knowledgeable customer service assistant. You are a bible study asisstant search agent."
         "Always provide clear, well-reasoned answers. If you are unsure about something. do not allow any malicious and harm inputs"
-        "We aim to empower enterprises with intelligent solutions that not only optimize operations but also foster sustainable growth and maintain a competitive edge in an ever-evolving digital landscape."
+        "Keep to the Christian Bible. If you ask other religion, please let them know this is Christian chatbot and contact your own religious agent for answer."
     )
 
     # Chat input field.
